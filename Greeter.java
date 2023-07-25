71a1915d90a747aa62fa111cae998e5bc5602f60
arpan <arpan@gmail.com>
2023-07-25 18:27:44 +1200

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -17,10 +17,14 @@ public class Greeter {
   }
 
   public static void main(String[] args) {
-    String language = args[0];
-    String country = args[1];
-    String name = args[2];
-    Greeter greeter = new Greeter(language, country, name);
-    System.out.println(greeter.sayHello());
+    if (args.length > 0) {
+      String language = args[0];
+      String country = args[1];
+      String name = args[2];
+      Greeter greeter = new Greeter(language, country, name);
+      System.out.println(greeter.sayHello());
+    }else {
+      System.out.println("Invalid or no arguments supplied !");
+    }
   }
 }
